af8dac19a0563e95ec136cadbf7e37aac5885954
scherzer <scherzer@hs-weingarten.de>
2021-03-14 11:34:33 +0100

    quick commit

--- Tests/FrameBufferHelperTests.cs
+++ Tests/FrameBufferHelperTests.cs
@@ -24,7 +24,7 @@ namespace Zenseless.OpenTK.Tests
 			window.Close();
 			return result;
 		}
-#if !APPVEYOR
+
 		[TestMethod()]
 		public void ToByteArrayTest()
 		{
@@ -39,6 +39,5 @@ namespace Zenseless.OpenTK.Tests
 			//var settings = new PixelReadSettings(size, size, StorageType.Char, PixelMapping.RGB);
 			//using var image = new MagickImage(buffer, settings);
 		}
-#endif
 	}
 }
