3b31f9424da8354c01fbae9642f68124a655f6ec
Daniel Scherzer <scherzer@rwu.de>
2021-09-22 16:24:18 +0200

    quick commit

--- Tests/Texture2DLoaderTests.cs
+++ Tests/Texture2DLoaderTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenTK.Graphics.OpenGL4;
 using Zenseless.Resources;
 
 namespace Zenseless.OpenTK.Tests
@@ -16,6 +17,8 @@ namespace Zenseless.OpenTK.Tests
 				var tex = Texture2DLoader.Load(stream);
 				Assert.AreEqual(1024, tex.Width);
 				Assert.AreEqual(1024, tex.Height);
+				GL.GetTextureLevelParameter(tex.Handle, 0, GetTextureParameter.TextureInternalFormat, out int format);
+				Assert.AreEqual((int)SizedInternalFormat.Rg8, format);
 				tex.Dispose();
 				return 0;
 			});
