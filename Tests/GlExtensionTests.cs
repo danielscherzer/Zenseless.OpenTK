77461e0f23b4b5127423a15fdf7cb2f1e14748a0
Daniel Scherzer <scherzer@rwu.de>
2022-06-13 13:06:04 +0200

    quick commit

---
+++ Tests/GlExtensionTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Zenseless.OpenTK.Tests
+{
+	[TestClass()]
+	public class GlExtensionTests
+	{
+		[TestMethod(), TestCategory("OpenGL")]
+		public void IsSupportedTest()
+		{
+			var result = Helper.ExecuteOnOpenGL(window =>
+			{
+				// query some extension every hardware supports
+				return GlExtension.IsSupported("GL_ARB_FRAGMENT_SHADER") && GlExtension.IsSupported("gl_arb_fragment_shader");
+			});
+			Assert.IsTrue(result);
+		}
+	}
+}
