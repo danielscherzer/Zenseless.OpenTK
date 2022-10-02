35e40959b813d32da99539b65816337d88706fb8
Daniel Scherzer <scherzer@rwu.de>
2022-10-02 10:20:19 +0200

    quick commit

--- src/Transformation2d.cs
+++ src/Transformation2d.cs
@@ -14,6 +14,7 @@ namespace Zenseless.OpenTK
 		/// <param name="applyToCoordinatesSecond">A transformation.</param>
 		/// <returns></returns>
 		public static Matrix4 Combine(Matrix4 applyToCoordinatesFirst, Matrix4 applyToCoordinatesSecond) => applyToCoordinatesFirst * applyToCoordinatesSecond;
+		
 		/// <summary>
 		/// Calculates the combined transformation by applying each transformation to input points.
 		/// </summary>
@@ -28,12 +29,14 @@ namespace Zenseless.OpenTK
 			}
 			return result;
 		}
+
 		/// <summary>
 		/// Creates a 2d rotation transformation.
 		/// </summary>
 		/// <param name="angleRadiant">the angle to rotate in radiants</param>
 		/// <returns></returns>
 		public static Matrix4 Rotation(float angleRadiant) => Matrix4.CreateRotationZ(angleRadiant);
+
 		/// <summary>
 		/// Create a 2d scale transformation
 		/// </summary>
@@ -41,18 +44,21 @@ namespace Zenseless.OpenTK
 		/// <param name="sy">y-scale factor</param>
 		/// <returns></returns>
 		public static Matrix4 Scale(float sx, float sy) => Matrix4.CreateScale(sx, sy, 1f);
+
 		/// <summary>
 		/// Create a 2d scale transformation
 		/// </summary>
 		/// <param name="scale">the scale factors</param>
 		/// <returns></returns>
 		public static Matrix4 Scale(Vector2 scale) => Scale(scale.X, scale.Y);
+
 		/// <summary>
 		/// Create a 2d scale transformation
 		/// </summary>
 		/// <param name="uniformScaleFactor">scale factor for x and y axis</param>
 		/// <returns></returns>
 		public static Matrix4 Scale(float uniformScaleFactor) => Matrix4.CreateScale(uniformScaleFactor);
+
 		/// <summary>
 		/// Creates a 2d translation transform.
 		/// </summary>
@@ -60,12 +66,14 @@ namespace Zenseless.OpenTK
 		/// <param name="ty">the translation factor in y-direction.</param>
 		/// <returns></returns>
 		public static Matrix4 Translate(float tx, float ty) => Matrix4.CreateTranslation(tx, ty, 0f);
+
 		/// <summary>
 		/// Creates a 2d translation transform.
 		/// </summary>
 		/// <param name="translation">The translation vector</param>
 		/// <returns></returns>
 		public static Matrix4 Translate(Vector2 translation) => Translate(translation.X, translation.Y);
+
 		/// <summary>
 		/// Transform the given input location vector by the given transformation
 		/// </summary>
@@ -73,6 +81,7 @@ namespace Zenseless.OpenTK
 		/// <param name="transformation">the transformation to apply</param>
 		/// <returns></returns>
 		public static Vector2 Transform(this Vector2 input, Matrix4 transformation) => Vector4.TransformRow(new Vector4(input.X, input.Y, 0f, 1f), transformation).Xy;
+
 		/// <summary>
 		/// Transform the given input direction vector by the given transformation
 		/// </summary>
