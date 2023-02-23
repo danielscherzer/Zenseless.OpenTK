cb9fde27f7c37399ce8cf23c861691f1aa52c150
Daniel Scherzer <scherzer@rwu.de>
2023-02-23 13:56:57 +0100

    quick commit

--- src/Texture/Texture2DLoader.cs
+++ src/Texture/Texture2DLoader.cs
@@ -2,42 +2,51 @@
 using OpenTK.Graphics.OpenGL4;
 using System.IO;
 
-namespace Zenseless.OpenTK
+namespace Zenseless.OpenTK;
+
+/// <summary>
+/// Class for loading textures from images
+/// </summary>
+public static class Texture2DLoader
 {
 	/// <summary>
-	/// Class for loading textures from images
+	/// Load a texture from the given stream.
 	/// </summary>
-	public static class Texture2DLoader
+	/// <param name="stream">A stream containing an image.</param>
+	/// <returns>A Texture.</returns>
+	public static Texture2D Load(Stream stream)
 	{
-		/// <summary>
-		/// Load a texture from the given stream.
-		/// </summary>
-		/// <param name="stream">A stream containing an image.</param>
-		/// <returns>A Texture.</returns>
-		public static Texture2D Load(Stream stream)
+		using var image = new MagickImage(stream);
+		return Load(image);
+	}
+
+	/// <summary>
+	/// Load a texture from the given <seealso cref="MagickImage"/>.
+	/// </summary>
+	/// <param name="image">A <seealso cref="MagickImage"/>.</param>
+	/// <returns>A Texture.</returns>
+	public static Texture2D Load(MagickImage image)
+	{
+		var format = PixelFormat.Rgb;
+		var internalFormat = SizedInternalFormat.Rgb8;
+		switch (image.ChannelCount)
 		{
-			using var image = new MagickImage(stream);
-			var format = PixelFormat.Rgb;
-			var internalFormat = (SizedInternalFormat)All.Rgb8; //till OpenTK issue resolved on github https://github.com/opentk/opentk/issues/752
-			switch (image.ChannelCount)
-			{
-				case 2: format = PixelFormat.Rg; internalFormat = SizedInternalFormat.Rg8; break;
-				case 3: break;
-				case 4: format = PixelFormat.Rgba; internalFormat = SizedInternalFormat.Rgba8; break;
-				default: throw new InvalidDataException("Unexpected image format");
-			}
-			image.Flip();
-			var bytes = image.GetPixelsUnsafe().ToArray();
-			var texture = new Texture2D(image.Width, image.Height, internalFormat)
-			{
-				Function = TextureFunction.ClampToEdge,
-				MagFilter = TextureMagFilter.Linear,
-				MinFilter = TextureMinFilter.LinearMipmapLinear
-			};
-			GL.PixelStore(PixelStoreParameter.UnpackAlignment, 1); // some image sizes will cause memory acceptions otherwise
-			GL.TextureSubImage2D(texture.Handle, 0, 0, 0, image.Width, image.Height, format, PixelType.UnsignedByte, bytes);
-			GL.GenerateTextureMipmap(texture.Handle);
-			return texture;
+			case 2: format = PixelFormat.Rg; internalFormat = SizedInternalFormat.Rg8; break;
+			case 3: break;
+			case 4: format = PixelFormat.Rgba; internalFormat = SizedInternalFormat.Rgba8; break;
+			default: throw new InvalidDataException("Unexpected image format");
 		}
+		image.Flip();
+		var bytes = image.GetPixelsUnsafe().ToArray();
+		var texture = new Texture2D(image.Width, image.Height, internalFormat)
+		{
+			Function = TextureFunction.ClampToEdge,
+			MagFilter = TextureMagFilter.Linear,
+			MinFilter = TextureMinFilter.LinearMipmapLinear
+		};
+		GL.PixelStore(PixelStoreParameter.UnpackAlignment, 1); // some image sizes will cause memory acceptions otherwise
+		GL.TextureSubImage2D(texture.Handle, 0, 0, 0, image.Width, image.Height, format, PixelType.UnsignedByte, bytes);
+		GL.GenerateTextureMipmap(texture.Handle);
+		return texture;
 	}
 }
