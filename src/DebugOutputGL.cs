4f433d6f3533819795a018cb59872d2f42220367
Daniel Scherzer <scherzer@rwu.de>
2023-02-27 21:22:05 +0100

    quick commit

--- src/DebugOutputGL.cs
+++ src/DebugOutputGL.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using Zenseless.Patterns;
-using static OpenTK.Graphics.OpenGL.GL;
 
 namespace Zenseless.OpenTK
 {
