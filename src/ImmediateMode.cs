5d92798a727d3d982e32e8d04b39b77d51ba0a8d
Daniel Scherzer <scherzer@rwu.de>
2023-05-26 09:12:05 +0200

    quick commit

--- src/ImmediateMode.cs
+++ src/ImmediateMode.cs
@@ -1,16 +1,15 @@
 ﻿using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 
-namespace Zenseless.OpenTK
+namespace Zenseless.OpenTK;
+
+/// <summary>
+/// Contains helper settings to enable the old immediate mode for OpenGL
+/// </summary>
+public static class ImmediateMode
 {
 	/// <summary>
-	/// Contains helper settings to enable the old immediate mode for OpenGL
+	/// Returns an instance of <see cref="NativeWindowSettings"/> for immediate mode rendering
 	/// </summary>
-	public static class ImmediateMode
-	{
-		/// <summary>
-		/// Returns an instance of <see cref="NativeWindowSettings"/> for immediate mode rendering
-		/// </summary>
-		public static NativeWindowSettings NativeWindowSettings => new() { Flags = ContextFlags.Default, Profile = ContextProfile.Compatability };
-	}
+	public static NativeWindowSettings NativeWindowSettings => new() { Flags = ContextFlags.Default, Profile = ContextProfile.Compatability };
 }
